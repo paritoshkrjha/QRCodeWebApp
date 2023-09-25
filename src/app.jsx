14834162ba455225c9da00b8fe74536816eecff2
Paritosh Jha <paritoshjhaparitoshjha0504@gmail.com>
2023-09-25 13:19:57 +0530

    Added Fetch geolocation

--- src/app.jsx
+++ src/app.jsx
@@ -32,6 +32,7 @@ export function App() {
     const url = window.location.search;
     const urlParams = new URLSearchParams(url)
     const userId = urlParams.get('userId')
+    getCurrentUserLocation()
     if (!userId) {
       setInvalidUserId(true)
     }
@@ -39,6 +40,31 @@ export function App() {
       setUserId(userId)
     }
   }, [])
+
+
+  function getCurrentUserLocation(){
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        // Success callback function
+        (position) => {
+          // Get the user's latitude and longitude coordinates
+          const lat = position.coords.latitude;
+          const lng = position.coords.longitude;
+    
+          // Do something with the location data, e.g. display on a map
+          console.log(`Latitude: ${lat}, longitude: ${lng}`);
+        },
+        // Error callback function
+        (error) => {
+          // Handle errors, e.g. user denied location sharing permissions
+          console.error("Error getting user location:", error);
+        }
+      );
+    } else {
+      // Geolocation is not supported by the browser
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }
 
   async function recaptchaCallback(number) {
     const recaptcha = new RecaptchaVerifier(auth, 'recaptcha-container', {
